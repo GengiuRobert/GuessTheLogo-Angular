08c34600a6573c859b0a71d1ffd06cef50285cc3
GengiuRobert <fncrobert3@gmail.com>
2024-12-06 14:05:28 +0200

    update settings from the form and forward them to the service

--- guessthelogo/src/app/home/home.component.ts
+++ guessthelogo/src/app/home/home.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { HomeService } from '../../services/home.service';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  public difficulty = signal<number>(1);
+  public category = signal<string>('default');
+
+  constructor(private homeService: HomeService) {
+
+  }
+
+  onSubmit() {
+    this.homeService.updateSettings(this.category(), this.difficulty());
+  }
 
 }
---
+++ guessthelogo/src/models/setting.model.ts
@@ -0,0 +1,4 @@
+export interface Setting{
+    category : string,
+    levelOfDifficulty: number
+}
